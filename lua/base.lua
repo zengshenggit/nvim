e4d0d637748dddc9f26f9d1712a5d684b5606832
jfbeatit <ctwpxz@163.com>
2022-11-19 14:07:46 +0800

    chore: update base options

--- lua/base.lua
+++ lua/base.lua
@@ -11,9 +11,9 @@ local define_sign = function(opts)
 end
 
 local function define_options(options)
-    for _, val in pairs(options) do
-        vim.o[_] = val
-    end
+  for _, val in pairs(options) do
+    vim.o[_] = val
+  end
 end
 
 vim.g.mapleader = ';'
@@ -21,6 +21,9 @@ vim.g.nobackup = true
 vim.g.nowritebackup = true
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.cmd([[
+  autocmd FileType php set shiftwidth=4
+]])
 
 define_sign({name = 'DiagnosticSignError', text = ''})
 define_sign({name = 'DiagnosticSignWarn', text = ''})
@@ -28,40 +31,40 @@ define_sign({name = 'DiagnosticSignHint', text = ''})
 define_sign({name = 'DiagnosticSignInfo', text = ''})
 
 define_options({
-    termguicolors = true,
-    number = true,
-    smartcase = true,
-    wrap = true,
-    ic = true,
-    autochdir = true,
-    autoindent = true,
-    smartindent = true,
-    expandtab = true,
-    showcmd = true,
-    showmatch = true,
-    ruler = true,
-    lazyredraw = true,
-    visualbell = true,
-    hlsearch = true,
-    incsearch = true,
-    list = true,
-    wildmenu = true,
-    relativenumber = true,
-    hidden = true,
-    clipboard = 'unnamedplus',-- use stystem's clipboard , install 'xclip' first
-    encoding = 'utf-8',
-    fileformats = 'unix',
-    mouse = 'nv',
-    tabstop = 2,
-    shiftwidth = 2,
-    history = 800,
-    scrolloff = 5,
-    wildmode = 'full',
-    foldmethod = 'marker',
-    foldlevelstart = 99,
-    laststatus = 3, -- nvim0.7 feature - for global status line
-    signcolumn = 'yes',
-    showtabline = 1,
-    updatetime = 100,
-    cmdheight = 1
+  termguicolors = true,
+  number = true,
+  smartcase = true,
+  wrap = true,
+  ic = true,
+  autochdir = true,
+  autoindent = true,
+  smartindent = true,
+  expandtab = true,
+  showcmd = true,
+  showmatch = true,
+  ruler = true,
+  lazyredraw = true,
+  visualbell = true,
+  hlsearch = true,
+  incsearch = true,
+  list = true,
+  wildmenu = true,
+  relativenumber = true,
+  hidden = true,
+  clipboard = 'unnamedplus',-- use stystem's clipboard , install 'xclip' first
+  encoding = 'utf-8',
+  fileformats = 'unix',
+  mouse = 'nv',
+  tabstop = 2,
+  shiftwidth = 2,
+  history = 800,
+  scrolloff = 5,
+  wildmode = 'full',
+  foldmethod = 'marker',
+  foldlevelstart = 99,
+  laststatus = 3, -- nvim0.7 feature - for global status line
+  signcolumn = 'yes',
+  showtabline = 1,
+  updatetime = 100,
+  cmdheight = 1
 })
