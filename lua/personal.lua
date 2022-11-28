da2745f64f1b6f527f3feba125a8c89b5203ccb7
jfbeatit <ctwpxz@163.com>
2022-11-28 14:52:00 +0800

    feat: personal custom commands

---
+++ lua/personal.lua
@@ -0,0 +1,14 @@
+local api = vim.api
+
+api.nvim_create_user_command('Pd', function()
+  local cmd = "cd /workspace/www/mcenter/frontend && pnpm dev"
+  vim.fn.jobstart(cmd, {
+    detach = false,
+    pty = true,
+    on_stdout = function(job_id, data, event)
+      if event == 'stdout' then
+        vim.notify('pnpm dev start...')
+      end
+    end
+  })
+end, {})
