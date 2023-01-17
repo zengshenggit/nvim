12d092ffb055c26789a188ace3c095f01ed1f926
jfbeatit <ctwpxz@163.com>
2023-01-17 15:47:36 +0800

    pref: lazy load plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -84,7 +84,6 @@ require('packer').startup(function(use)
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
     requires = { {'nvim-lua/plenary.nvim'} },
-    ft = filetypes
   }
   use {
     'voldikss/vim-floaterm',
